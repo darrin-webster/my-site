9a54a4e1ba403cb5f82208b626780b7f06850da0
Darrin Webster <dw@hf.cx>
2019-01-22 12:59:12 -0800

    Completed the final part of the begginer Django tutorial. Moving on to advanced

--- polls/models.py
+++ polls/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 
 # class qeustion
 class Question(models.Model):
-	#form field for question; a character field not to exceed 200 chars
+    #form field for question; a character field not to exceed 200 chars
     question_text = models.CharField(max_length=200)
     # Logs date when published
     pub_date = models.DateTimeField('date published')
@@ -16,6 +16,9 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        was_published_recently.admin_order_field = 'pub_date'
+        was_published_recently.boolean = True
+        was_published_recently.short_description = 'Published recently?'
 
     
 # class choice
